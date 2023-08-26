f13b9cef5384fa4a1aa2a3a0dff3c3d6817329d0
Saymon Damásio <saymon.damasio95@gmail.com>
2023-08-26 17:06:05 -0300

    feat: add create transaction route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,13 +1,28 @@
 import { knex } from '../database'
 
+import { z } from 'zod'
+
 import { FastifyInstance } from 'fastify'
+import { randomUUID } from 'crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transaction = await knex('transactions')
-      .where('amount', 1000)
-      .select('*')
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transaction
+    const { amount, title, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
